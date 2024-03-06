2d278fc55c87916e7d64af45f757501cfd51f695
Junaidahmed2319 <161757925+Junaidahmed2319@users.noreply.github.com>
2024-03-06 22:51:08 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,4 @@
+let name1 = "junaid";
+console.log(name1.toLowerCase()); // junaid
+console.log (name1.toUpperCase());// JUNAID
+console.log(name1.charAt(0).toUpperCase() + name1.slice(1).toLowerCase());
