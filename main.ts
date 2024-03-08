b9b79f04c243d4de2c222ad7f7e3df00bd62fd7b
Junaid Ahmed <161757925+Junaidahmed2319@users.noreply.github.com>
2024-03-08 23:25:04 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,7 @@
+
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Enclose your operations in print statements.
+
+console.log(5 + 3); // ADDITION
+console.log (128-120); // subtraction
+console.log(8 * 1); // multiplication
+console.log (64/8); // division
