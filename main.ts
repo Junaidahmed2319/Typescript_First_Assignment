06ec19fa77094f907b8f81997aaa5279f5613044
Junaid Ahmed <161757925+Junaidahmed2319@users.noreply.github.com>
2024-03-09 00:02:26 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,6 @@
+// Question 10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If the programs are straightforward at this point, just add your name and the current date at the top of each program file. Then, write one sentence describing what the program does.
+
+// Junaid Ahmed, 2024-03-08
+// This program prints a personal message.
+let myname = "Junaid Ahmed";
+console.log(`Hello ${myname}, would you like to learn some thing today?`);
