20e373e9ba705857e85952e3e4656ebbd0ea4a73
Junaid Ahmed <161757925+Junaidahmed2319@users.noreply.github.com>
2024-05-28 18:50:57 +0500

    Add files via upload

---
+++ Ans 14/main.js
@@ -0,0 +1,4 @@
+var guests = ["Abu Baker", "Umer", "Usman"];
+guests.forEach(function (guest) {
+    console.log("Dear ".concat(guest, ", Would you join us for dinner on Tuesday?"));
+});
---
+++ Ans 14/main.ts
@@ -0,0 +1,5 @@
+let guests: string[] = ["Abu Baker", "Umer", "Usman"];
+
+guests.forEach(guest => {
+console.log (`Dear ${guest}, Would you join us for dinner on Tuesday?`)
+});
