ade594c388f62ad9761960547c6c9398e2955737
Junaid Ahmed <161757925+Junaidahmed2319@users.noreply.github.com>
2024-05-28 18:42:09 +0500

    Add files via upload

---
+++ Ans 13/main.js
@@ -0,0 +1,4 @@
+var transports = ["Yamaha motorcycle", "Ford car", "Giant bicycle"];
+transports.forEach(function (transport) {
+    console.log("I would like to own a ".concat(transport, "."));
+});
---
+++ Ans 13/main.ts
@@ -0,0 +1,5 @@
+let transports: string[] = ["Yamaha motorcycle", "Ford car", "Giant bicycle"];
+
+transports.forEach(transport => {
+    console.log(`I would like to own a ${transport}.`);
+});
